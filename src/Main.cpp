ceb1275ab30a5cb56c14def58fdb182c2e084ac4
Jacques Visser <jacquesvisserjnr@gmail.com>
2021-04-12 16:21:50 -0700

    Started From Scratch

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,64 @@
-#include "Platform/Platform.hpp"
-#include <Game.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Main.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int ScreenX = 800;
+int ScreenY = 800;
+
+std::string ASSETS = "content/";
+std::string SPRITES = "sprites/";
+std::string FONTS = "fonts/";
+
+sf::Vector2f playerPos = nullptr;
 
 int main()
 {
-	Game game;
-	game.Run();
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(ScreenX, ScreenY), "Asteroids");
+
+	// Load a sprite to display
+	sf::Texture texture;
+	if (!texture.loadFromFile(ASSETS + SPRITES + "Player.png"))
+		return EXIT_FAILURE;
+	sf::Sprite sprite(texture);
+
+	sprite.setOrigin(texture.getSize().x / 2, texture.getSize().y / 2);
+	sprite.setPosition(ScreenX / 2, ScreenY / 2);
+	sprite.setScale(0.5f, 0.5f);
+
+	// Create a graphical text to display
+	sf::Font font;
+	if (!font.loadFromFile(ASSETS + FONTS + "Teko-Regular.ttf"))
+		return EXIT_FAILURE;
+	sf::Text text("Hello SFML", font, 50);
+
+	// Open game window
+	while (window.isOpen())
+	{
+		// Process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window: exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		// Clear screen
+		window.clear();
+		// Draw the sprite
+		window.draw(sprite);
+		// Draw the string
+		window.draw(text);
+		// Update the window
+		window.display();
+	}
+	return EXIT_SUCCESS;
 }
