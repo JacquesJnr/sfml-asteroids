83337a52cfb8c68a2a631ab929cf35cbe970daae
Jacques Visser <jacquesvisserjnr@gmail.com>
2021-04-11 17:46:32 -0700

    Revert "removed some platform specific files from original copy"
    This reverts commit 5f07031212f98f84076cadf5292a8db02a407081.

--- src/Platform/Platform.hpp
+++ src/Platform/Platform.hpp
@@ -1,13 +1,21 @@
 #ifndef UTIL_PLATFORM_HPP
 #define UTIL_PLATFORM_HPP
 
-#if defined(_WIN32)
+#if defined(__APPLE__)
+	#include "Platform/MacOS/MacOSPlatform.hpp"
+#elif defined(__linux__)
+	#include "Platform/Unix/LinuxPlatform.hpp"
+#elif defined(_WIN32)
 	#include "Platform/Win32/WindowsPlatform.hpp"
 #endif
 
 namespace util
 {
-#if defined(_WIN32)
+#if defined(__APPLE__)
+using Platform = MacOSPlatform;
+#elif defined(__linux__)
+using Platform = LinuxPlatform;
+#elif defined(_WIN32)
 using Platform = WindowsPlatform;
 #endif
 }
